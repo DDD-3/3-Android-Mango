53f3da8be8e3119f42cd191ffdd754b81acebbfd
jinho <jinho6187@gmail.com>
2019-11-28 17:39:23 +0900

    BaseActivity 수정

--- app/src/main/java/com/mango/ui/main/MainActivity.kt
+++ app/src/main/java/com/mango/ui/main/MainActivity.kt
@@ -1,21 +1,16 @@
 package com.mango.ui.main
 
-import android.os.Bundle
-import androidx.databinding.DataBindingUtil
 import com.mango.R
 import com.mango.base.BaseActivity
 import com.mango.databinding.ActivityMainBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class MainActivity : BaseActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
-    private lateinit var binding: ActivityMainBinding
     private val viewModel by viewModel<MainViewModel>()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    override fun initView() {
         binding.viewModel = viewModel
-        binding.lifecycleOwner = this
     }
+
 }
